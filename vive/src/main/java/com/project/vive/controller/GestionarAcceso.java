126f490fcaede227a047834321cd5537e8f40e4a
JuanDanielCR <castilloreyesjuan@gmail.com>
2017-06-25 18:28:10 -0500

    Agregando Dashboard

--- vive/src/main/java/com/project/vive/controller/GestionarAcceso.java
+++ vive/src/main/java/com/project/vive/controller/GestionarAcceso.java
@@ -3,6 +3,7 @@ package com.project.vive.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/login")
@@ -10,5 +11,9 @@ public class GestionarAcceso {
 	@RequestMapping(method=RequestMethod.GET)
 	public String login(){
 		return "login";
+	}
+	@RequestMapping(method=RequestMethod.POST)
+	public RedirectView verificarLogin(){
+		return new RedirectView("redirect:/menu?nombre='Juan Castillo'");
 	}
 }
