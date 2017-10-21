f5cae04cf840e49d2a165ed241d768e4cd4b5772
Huitzoo <eckobik@gmail.com>
2017-10-21 12:26:39 -0500

    cliente

---
+++ cliente/urls.py
@@ -0,0 +1,34 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    url(
+        regex=r'^registrar/$',
+        view=views.agregarCliente,
+        name='agregarCliente'
+    ),
+    url(
+        regex=r'^/(?P<idAsesorCliente>\d+)/$',
+        view=views.clientes,
+        name='clientes'
+    ),
+    url(
+        regex=r'^gestionar/$',
+        view=views.gestionarClientes,
+        name='gestionarCliente'
+    ),
+    url(
+        regex=r'^editar/(?P<idAsesorCliente>\d+)$',
+        view=views.editar,
+        name='editar'
+    ),
+    url(
+        regex=r'^eliminar/(?P<idAsesorCliente>\d+)$',
+        view=views.eliminar,
+        name='eliminar'
+    ),
+    url(
+        regex=r'^hola/$',
+        view=views.hola,
+        name='hola'
+    ),
+]
