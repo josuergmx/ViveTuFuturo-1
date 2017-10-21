ec2f1f95d1d46344db036f3da210d4f407f8d47c
JuanDanielCR <castilloreyesjuan@gmail.com>
2017-10-21 11:12:40 -0500

    Borrando archivos inncesarios en maqueta

--- vive/src/main/java/com/project/vive/service/impl/ContactoImpl.java
+++
@@ -1,8 +0,0 @@
-package com.project.vive.service.impl;
-
-import com.project.vive.entity.Contacto;
-
-public interface ContactoImpl {
-	//TODO: hacer model
-	public abstract Contacto guardar(Contacto model);
-}
