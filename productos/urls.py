3c39824ee9d194de7bc38e8bde921a5202c66562
JuanDanielCR <castilloreyesjuan@gmail.com>
2017-11-04 14:30:04 -0600

    urls producto

--- productos/urls.py
+++ productos/urls.py
@@ -1,2 +1,9 @@
 from django.conf.urls import url
 from . import views
+
+urlpatterns = [
+    url(regex = r'',view=views.gestionr,name='gestionarProducto'),
+    url(regex = r'',view=views.gestionr,name='agregarProducto'),
+    url(regex = r'',view=views.gestionr,name='editarProducto'),
+    url(regex = r'',view=views.gestionr,name='eliminarProducto'),
+]
