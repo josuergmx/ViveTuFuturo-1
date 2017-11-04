cbcbf3924a8859cfcfbf854fe133aca393819eca
JuanDanielCR <castilloreyesjuan@gmail.com>
2017-11-04 15:14:10 -0600

    Migratiosn

--- productos/urls.py
+++ productos/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from . import views
 
-<<<<<<< HEAD
 urlpatterns = [
     url(regex = r'',view=views.gestionarProducto,name='gestionarProducto'),
     url(regex = r'',view=views.agregarProducto,name='agregarProducto'),
